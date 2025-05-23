20a63ecdb7ca3ec68f0e37bd93552ac5324bdbe8
Dynamo <chickabamboombam@gmail.com>
2025-05-23 16:11:33 -0400

    Updated Workspace/Claw StarterCharacter (SCRIPTED)/Directions.lua

---
+++ Workspace/Claw StarterCharacter (SCRIPTED)/Directions.lua
@@ -0,0 +1,13 @@
+-- @ScriptType: Script
+--[[
+Asset: Claw StarterCharacter
+Created by: Roller_Bott
+5/22/2025
+
+DIRECTIONS:
+--> Put each folder in the service that shares its name and ungroup it
+--> Change animations and sounds in the folders under the ClawServer script
+--> That's it! Enjoy!
+
+
+]]
